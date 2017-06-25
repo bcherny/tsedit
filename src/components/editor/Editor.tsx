9ebd48291e448abae759965ecf725b6ede645097
Boris Cherny <boris@performancejs.com>
2017-06-25 14:13:52 -0700

    get editor working

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,4 +1,3 @@
-import 'monaco-editor/min/vs/editor/editor.main.js'
 import * as React from 'react'
 
 type Props = {
@@ -7,37 +6,64 @@ type Props = {
 
 type State = {
   editor?: monaco.editor.IStandaloneCodeEditor
+  isLoadingEditor: boolean
   text: string
 }
 
 export class Editor extends React.Component<Props, State> {
-  render() {
-    return <div ref={div => this.initMonaco(div)} />
+
+  state: State = {
+    isLoadingEditor: false,
+    text: ''
   }
+
+  render() {
+    return <div>
+      <div className='Editor' ref={div => this.initMonaco(div)} />
+      <button className='Button RunButton' onClick={this.run}>Run</button>
+    </div>
+  }
+
+  run = () =>
+    this.props.onChange(this.state.text)
+
   onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
     this.setState({ text: e.currentTarget.value })
 
   onKeyDown = (e: monaco.IKeyboardEvent) => {
+    console.log('onkey', e.keyCode, e.metaKey)
     if (e.keyCode === monaco.KeyCode.Enter && e.metaKey) {
-      this.props.onChange(this.state.text)
+      this.run()
     }
   }
 
   initMonaco(div: HTMLDivElement) {
+    if (!div || this.state.editor || this.state.isLoadingEditor) {
+      return
+    }
+    this.setState({ isLoadingEditor: true });
     (window as any).require(['vs/editor/editor.main'], () => {
-      if (typeof monaco !== 'undefined') {
-        let editor = monaco.editor.create(div, {
-          value: this.state.text,
-          language: 'javascript'
-        })
-        editor.onKeyDown(this.onKeyDown)
-        editor.onDidChangeModelContent(e => {
-          this.setState({
-            text: e.text
-          })
-        })
-        this.setState({ editor })
-      }
+      let editor = monaco.editor.create(div, {
+        value: this.state.text,
+        language: 'javascript'
+      })
+
+      let modifier = editor.createContextKey('modifier', false)
+
+      editor.addCommand(monaco.KeyCode.Enter, function() {
+        console.log('command enter')
+      }, 'modifier')
+
+      editor.addCommand(monaco.KeyCode.Meta, e => {
+        console.log('meta', e)
+        modifier.set(true)
+      }, '')
+
+      // editor.onKeyUp(this.onKeyDown)
+      editor.onDidChangeModelContent(({ text }) =>
+        this.setState({ text })
+      )
+      this.setState({ editor, isLoadingEditor: false })
     })
   }
   componentWillUnmount(){
